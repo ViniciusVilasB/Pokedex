8f6d179b456469994662d5636115c18821e7f602
Vinicius Vilas Boas <boas.vini@gmail.com>
2024-10-24 15:43:49 -0300

    Segundo commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pokedex
+# Portal Filmes
--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,6 @@ const router= createBrowserRouter([
       {index: true, element: <Home />},
       {path: '*', element: <PageNotFound/>}
     ]
-    
   }
 ])
 
